55efda97d0017d0b6af5e2828812afdc4b83c8fd
PanhaBo <panhabo.nova@gmail.com>
2025-05-24 16:55:11 +0700

    2 Factors Verification

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('two_factor_code')->nullable();
+            $table->timestamp('two_factor_expires_at')->nullable();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\AddressController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TwoFactorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -42,9 +44,8 @@ Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.s
 
 
 // auth_form
-Route::get('/register',function(){
-    return view('authentication_form.register');
-})->name('register');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/login',function(){
     return view('authentication_form.login');
@@ -70,4 +71,11 @@ Route::get('/productdetail',function(){
 
 
 Route::get('/productdetail', [ProductDetailController::class, 'index'])->name('productdetail');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/two-factor', [TwoFactorController::class, 'index'])->name('two_factor.index');
+    Route::post('/two-factor', [TwoFactorController::class, 'verify'])->name('two_factor.verify');
+});
 
